b8286841753a26c8f418639b88d2f9a01dc01537
Luis Amaya <prosperitylife207@gmail.com>
2021-10-04 20:49:29 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+// setup template engine
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set ('view engine', 'handlebars');
+// setup express static public folder for css,js, and images
+app.use(express.static('public'));
+// setup enviroment variables
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
+});
